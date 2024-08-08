201f65e2c3868f835b1155415ce9c6b40da7a2fb
ronniemoyo <moyoronnie1@gmail.com>
2024-08-08 12:33:09 +0200

    fixing errors

--- task_5.sql
+++ task_5.sql
@@ -2,6 +2,6 @@
 
 -- This script inserts a single row into the 'customer' table in the 'alx_book_store' database.
 
-INSERT INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO Customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
 
--- task_6.sql
+++ task_6.sql
@@ -1,6 +1,6 @@
 -- This script inserts multiple rows into the 'customer' table in the 'alx_book_store' database.
 
-INSERT INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES 
 (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
 (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
